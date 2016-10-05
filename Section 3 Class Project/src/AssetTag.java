7d90f3a94f4b4ce06644277a0564f7e021502713
Dandersen97 <dandersen@stumail.fhtc.edu>
2016-10-05 08:22:50 -0500

    Saving daily work

--- Section 3 Class Project/src/AssetTag.java
+++ Section 3 Class Project/src/AssetTag.java
@@ -1,3 +1,5 @@
+// Devon Andersen
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -67,12 +69,15 @@ public class AssetTag {
 		assets.add("0002020OTHDCDC10500091820120202DEF");
 		assets.add("0008511TBTMNM104 00022920150101BAD");
 		assets.add("8675309OTHMNM113 00063620140611DAD");
-		
-		
-		TagCut(assets[1]);
-		print();
-		
-		
+				
+		for(String a : assets){
+			if(a.matches("^[0-9]{7}[A-Z]{5}[0-9A-Z ]{5}[0-9A-Z]{14}[0-9A-F]{3}$")){
+				TagCut(a);
+				print();
+			}
+			
+		}
+
 	}
 	
 	public static void TagCut(String tag) {
@@ -91,41 +96,62 @@ public class AssetTag {
 		System.out.println("PO: " + getPO());
 		System.out.println("Tagged Date: " + getTagDate());
 		System.out.println("Checksum: " + getChecksum());
+		System.out.println("--------------------------------");
 	}
-	
-	
-	
-
-	
-	
-	
 	
 	
 	
 	
 	private static String DBId;
 	public static String getDBId() {return DBId;}
-	public static void setDBId(String i) {i = i.replaceAll("^0+", "");     DBId = i;}
+	public static void setDBId(String i) {
+		i = i.replaceAll("^0+", "");
+		DBId = i;}
+	
 	
 	private static String Device;
 	public static String getDevice() {return Device;}
-	public static void setDevice(String d) {Device = d;}
+	public static void setDevice(String d) {
+		if(d.matches("^[C][M][P]$")){d = "Computer";}
+		else if(d.matches("^[L][P][T]$")){d = "Laptop";}
+		else if(d.matches("^[P][R][N]$")){d = "Printer";}
+		else if(d.matches("^[P][R][J]$")){d = "Projector";}
+		else if(d.matches("^[T][B][T]$")){d = "Tablet";}
+		else if(d.matches("^[P][H][N]$")){d = "Phone";}
+		else if(d.matches("^[O][T][H]$")){d = "Other";}
+		Device = d;}
+	
 	
 	private static String Building;
 	public static String getBuilding() {return Building;}
-	public static void setBuilding(String b) {Building = b;}
+	public static void setBuilding(String b) {
+		if(b.matches("^[M][N]$")){b = "Main Campus";}
+		else if(b.matches("^[S][A]$")){b = "Sauder Extension Building";}
+		else if(b.matches("^[D][C]$")){b = "Downtown Campus";}
+		else if(b.matches("^[W][D]$")){b = "Welder Building";}
+		else if(b.matches("^[T][C]$")){b = "Technology Building";}
+		else if(b.matches("^[J][E]$")){b = "Jones Education Building";}
+		Building = b;}
+	
 	
 	private static String Room;
 	public static String getRoom() {return Room;}
 	public static void setRoom(String r) {Room = r;}
 	
+	
 	private static String PO;
 	public static String getPO() {return PO;}
-	public static void setPO(String p) {p = p.replaceAll("^0+", "");     PO = p;}
+	public static void setPO(String p) {
+		p = p.replaceAll("^0+", "");
+		PO = p;}
+	
 	
 	private static String TagDate;
 	public static String getTagDate() {return TagDate;}
-	public static void setTagDate(String t) {t = t.substring(4, 6) + "/" + t.substring(6, 8) + "/" + t.substring(0, 4);      TagDate = t;}
+	public static void setTagDate(String t) {
+		t = t.substring(4, 6) + "/" + t.substring(6, 8) + "/" + t.substring(0, 4);
+		TagDate = t;}
+	
 	
 	private static String Checksum;
 	public static String getChecksum() {return Checksum;}
